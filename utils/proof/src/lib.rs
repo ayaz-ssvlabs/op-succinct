30ec250e80f2e495e534d0e5a1f78f85d14e12c7
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2025-09-17 20:33:31 +0100

    force using generated efl

--- utils/proof/src/lib.rs
+++ utils/proof/src/lib.rs
@@ -1,20 +1,22 @@
 use std::sync::Arc;
 
 use op_succinct_host_utils::fetcher::OPSuccinctDataFetcher;
+ use op_succinct_elfs::RANGE_ELF_EMBEDDED;
 
 /// Get the range ELF depending on the feature flag.
 pub fn get_range_elf_embedded() -> &'static [u8] {
-    cfg_if::cfg_if! {
-        if #[cfg(feature = "celestia")] {
-            use op_succinct_elfs::CELESTIA_RANGE_ELF_EMBEDDED;
+    // cfg_if::cfg_if! {
+    //     if #[cfg(feature = "celestia")] {
+    //         use op_succinct_elfs::CELESTIA_RANGE_ELF_EMBEDDED;
 
-            CELESTIA_RANGE_ELF_EMBEDDED
-        } else {
-            use op_succinct_elfs::RANGE_ELF_EMBEDDED;
+    //         CELESTIA_RANGE_ELF_EMBEDDED
+    //     } else {
+    //         use op_succinct_elfs::RANGE_ELF_EMBEDDED;
 
-            RANGE_ELF_EMBEDDED
-        }
-    }
+    //         RANGE_ELF_EMBEDDED
+    //     }
+    // }
+    RANGE_ELF_EMBEDDED
 }
 
 cfg_if::cfg_if! {
