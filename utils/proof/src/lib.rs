cceea5e2eec174ca1f49dc843f9556139834c45a
ayaz-ssvlabs <ayaz@ssvlabs.io>
2025-09-23 16:50:36 +0500

    Use forked kona, remove celestia DA support to avoid compilation issues

--- utils/proof/src/lib.rs
+++ utils/proof/src/lib.rs
@@ -9,40 +9,15 @@ pub use logging::GuestLogBridge;
 
 /// Get the range ELF depending on the feature flag.
 pub fn get_range_elf_embedded() -> &'static [u8] {
-    // cfg_if::cfg_if! {
-    //     if #[cfg(feature = "celestia")] {
-    //         use op_succinct_elfs::CELESTIA_RANGE_ELF_EMBEDDED;
-
-    //         CELESTIA_RANGE_ELF_EMBEDDED
-    //     } else {
-    //         use op_succinct_elfs::RANGE_ELF_EMBEDDED;
-
-    //         RANGE_ELF_EMBEDDED
-    //     }
-    // }
     RANGE_ELF_EMBEDDED
 }
 
-cfg_if::cfg_if! {
-    if #[cfg(feature = "celestia")] {
-        use op_succinct_celestia_host_utils::host::CelestiaOPSuccinctHost;
+use op_succinct_ethereum_host_utils::host::SingleChainOPSuccinctHost;
 
-        /// Initialize the Celestia host.
-        pub fn initialize_host(
-            fetcher: Arc<OPSuccinctDataFetcher>,
-        ) -> Arc<CelestiaOPSuccinctHost> {
-            tracing::info!("Initializing host with Celestia DA");
-            Arc::new(CelestiaOPSuccinctHost::new(fetcher))
-        }
-    } else {
-        use op_succinct_ethereum_host_utils::host::SingleChainOPSuccinctHost;
-
-        /// Initialize the default (ETH-DA) host.
-        pub fn initialize_host(
-            fetcher: Arc<OPSuccinctDataFetcher>,
-        ) -> Arc<SingleChainOPSuccinctHost> {
-            tracing::info!("Initializing host with Ethereum DA");
-            Arc::new(SingleChainOPSuccinctHost::new(fetcher))
-        }
-    }
+/// Initialize the default (ETH-DA) host.
+pub fn initialize_host(
+    fetcher: Arc<OPSuccinctDataFetcher>,
+) -> Arc<SingleChainOPSuccinctHost> {
+    tracing::info!("Initializing host with Ethereum DA");
+    Arc::new(SingleChainOPSuccinctHost::new(fetcher))
 }
